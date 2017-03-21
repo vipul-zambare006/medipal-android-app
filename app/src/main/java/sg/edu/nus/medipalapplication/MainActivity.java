38adee0e744a23c99ade0043a488157ed14d802d
Gaurav Sharma <grv23390@gmail.com>
2017-03-21 15:22:17 +0000

    Update MainActivity.java

--- app/src/main/java/sg/edu/nus/medipalapplication/MainActivity.java
+++ app/src/main/java/sg/edu/nus/medipalapplication/MainActivity.java
@@ -1,16 +1,20 @@
 package sg.edu.nus.medipalapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import sg.edu.nus.medipalapplication.activity.AddAppointmentActivity;
-import sg.edu.nus.medipalapplication.activity.CategoryActivity;
-import sg.edu.nus.medipalapplication.activity.ConsumptionTabsActivity;
-import sg.edu.nus.medipalapplication.activity.ICETabLayoutActivity;
-import sg.edu.nus.medipalapplication.activity.MedicineActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
