ae7fd0a63e2b549cd751d7a42a59c5ea11232f41
Monalisa Debnath <monalisa93.debnath@gmail.com>
2017-03-22 08:11:34 +0000

    Add MedicalFragment

---
+++ app/src/main/java/sg/edu/nus/medipalapplication/fragment/MedicalFragment.java
@@ -0,0 +1,64 @@
+package sg.edu.nus.medipalapplication.fragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.example.monalisadebnth.myapplication_medical.R;
+import android.support.design.widget.FloatingActionButton;
+
+import activity.AddMedicalActivity;
+import adapter.MedicalListAdapter;
+
+/**
+ * Created by monalisadebnth on 19/3/17.
+ */
+
+public class MedicalFragment extends Fragment {
+
+    private MedicalListAdapter memberListAdapter;
+    private TextView tvEmpty;
+
+    public static MedicalFragment newInstance(){
+        MedicalFragment fragment = new MedicalFragment();
+        return fragment;
+    }
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                       Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fragmentView = inflater.inflate(R.layout.fragment_medical, container, false);
+        return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView memberList = (ListView) view.findViewById(R.id.lv_medical_list);
+        tvEmpty = (TextView) view.findViewById(R.id.tv_empty_value);
+        memberListAdapter = new MedicalListAdapter(getActivity());
+        memberList.setAdapter(memberListAdapter);
+        FloatingActionButton floatingActionButton =
+                (FloatingActionButton) view.findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), AddMedicalActivity.class));
+            }
+        });
+    }
+
+    @Override public void onResume() {
+        super.onResume();
+        memberListAdapter.refreshMembers();
+        tvEmpty.setVisibility(memberListAdapter.getCount() == 0 ? View.VISIBLE : View.GONE);
+    }
+}
