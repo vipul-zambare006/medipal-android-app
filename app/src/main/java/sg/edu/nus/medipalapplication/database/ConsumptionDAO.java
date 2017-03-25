8d3ce841ec099811107080e6d0fa6e0491db9f9a
RachelGnanaraj <e0146843@u.nus.edu>
2017-03-26 00:28:07 +0800

    ConsumptionDAO

--- app/src/main/java/sg/edu/nus/medipalapplication/database/ConsumptionDAO.java
+++ app/src/main/java/sg/edu/nus/medipalapplication/database/ConsumptionDAO.java
@@ -1,8 +1,44 @@
 package sg.edu.nus.medipalapplication.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.util.Log;
+
+import sg.edu.nus.medipalapplication.MedipalFolder.Consumption;
+
 /**
  * Created by Rach on 25/3/2017.
  */
 
-public class ConsumptionDAO {
+public class ConsumptionDAO extends DBHelper {
+
+    public ConsumptionDAO(Context context) {
+        super(context);
+    }
+
+    public boolean addConsumption(Consumption consumption) {
+        //consumption.getDate()
+        SQLiteDatabase db = this.getWritableDatabase();
+        try {
+            ContentValues contantValues = new ContentValues();
+            contantValues.put(Constant.MEDICINEID, consumption.getMedicineID());
+            contantValues.put(Constant.QUANTITY, consumption.getQuantity());
+            contantValues.put(Constant.CONSUMEDON, consumption.getDate());
+            db.insert(Constant.Consumption_Table_Name, null, contantValues);
+            return true;
+        } catch (SQLiteException e) {
+            Log.i("Consumption Added", Constant.ErrorMsg_RecordNotAdded);
+            return false;
+        } finally {
+            db.close();
+        }
+    }
+
+    /*public Cursor getAllConsumption(){
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor cursor=db.rawQuery(" select Medicinename from medicinetablecategory ",null);
+        return cursor;
+    }*/
 }
